1ec30dbc468cdd91bca843760ad1491109dda837
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-22 21:07:12 -0500

    Add Class example.

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    internal class Person
+    {
+        public string FirstName
+        { get; set; }
+        public string LastName
+        { get; set; }
+
+        public void Introduce()
+        {
+            System.Console.WriteLine("Hello, my name is {0} {1}", FirstName, LastName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,29 +6,37 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //  3) Type Conversion example
-            string s = "1";
-            int i = Convert.ToInt32(s);
-            int j = int.Parse(s);
-            Console.WriteLine("{0} {1} {2}", i, j, s);
+            //  4) Class example
+            var person = new Person();
+            person.FirstName = "Jonathan";
+            person.LastName = "Taylor";
+            person.Introduce();
 
-            i = 1000;
-            byte b = (byte)i;   //  data loss
-            Console.WriteLine(b);
-            try
-            {
-                var num = "1000";
-                b = Convert.ToByte(num);
-                Console.WriteLine(b);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("num could not be converted to byte");
-            }
+
+            //  3) Type Conversion example
+            //string s = "1";
+            //int i = Convert.ToInt32(s);
+            //int j = int.Parse(s);
+            //Console.WriteLine("{0} {1} {2}", i, j, s);
+
+            //i = 1000;
+            //byte b = (byte)i;   //  data loss
+            //Console.WriteLine(b);
+            //try
+            //{
+            //    var num = "1000";
+            //    b = Convert.ToByte(num);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("num could not be converted to byte");
+            //}
            
 
             //  2) Format String example
             //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
 
             // 1) Overflow example
             //byte number = 255;
