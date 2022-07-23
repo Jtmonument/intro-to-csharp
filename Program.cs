934a15b3620d1a23d0658ff04c83761fc6aa2877
Jonathan Taylor <jontaylor091@gmail.com>
2022-07-22 20:34:04 -0500

    Add type conversion example.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //  3) Type Conversion example
+            string s = "1";
+            int i = Convert.ToInt32(s);
+            int j = int.Parse(s);
+            Console.WriteLine("{0} {1} {2}", i, j, s);
+
+            i = 1000;
+            byte b = (byte)i;   //  data loss
+            Console.WriteLine(b);
+            try
+            {
+                var num = "1000";
+                b = Convert.ToByte(num);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("num could not be converted to byte");
+            }
+           
+
             //  2) Format String example
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
             // 1) Overflow example
             //byte number = 255;
             //number++; // overflows to 0
